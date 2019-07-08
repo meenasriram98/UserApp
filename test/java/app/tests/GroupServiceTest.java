128d3ec59bf12d527305dccba6b1684ac64ce260
meenasriram98 <meena.sriram@imaginea.com>
2019-07-08 12:36:13 +0530

    changed test file

--- test/java/app/tests/GroupServiceTest.java
+++ test/java/app/tests/GroupServiceTest.java
@@ -59,38 +59,5 @@ public class GroupServiceTest {
 		grouprepository.removeGroup("g5");
 		assertEquals(2,grouprepository.getSize());
 	}
-
-<<<<<<< Updated upstream
-	
-=======
->>>>>>> Stashed changes
-	@Test
-	public void testGroupPresent2() {
-		assertFalse(grouprepository.getGroup("meena"));
-	}
-
-@Test
-    public void testremoveGroupFromGroup()
-{
-	GroupService service=new GroupService();
-	Group gr=new Group("g6",19);
-	Group g=new Group("g7",3);
-	
-	grouprepository.addGroup(gr);
-	grouprepository.addGroup(g);
-	service.saveGroup(g.getName(), gr.getName());
-	
-	service.removeGroupFromGroup(gr.getName());
-	System.out.println(grouprepository.getGroupObject(g.getName()).getJoinedGroups().contains(gr));
-	System.out.println(grouprepository.getGroupObject(g.getName()).getJoinedGroups());
-	assertEquals(true,grouprepository.getGroupObject(g.getName()).getJoinedGroups().contains(gr));
-	
-}
-
-<<<<<<< Updated upstream
-    
-=======
-
->>>>>>> Stashed changes
     	
 }
